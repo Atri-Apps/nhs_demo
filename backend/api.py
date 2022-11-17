acb55a051de1a1e503d47ed2476a3e04f6e08c42
darshitac11 <darshitac11@gmail.com>
2022-11-16 19:28:12 -0500

    rewrote backend fns to include in main

--- backend/api.py
+++ backend/api.py
@@ -5,44 +5,58 @@ from collections import defaultdict
 import json
 
 
-def apply_dropdown_values(at: Atri):
-    json_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'data.json')
-    json_file = open(json_file_path)
-    data = json.load(json_file)
-    at.Dropdown1.custom.values = list(data.keys())
-    at.Dropdown1.custom.selectedValue = '100 general surgery service'
-    set_filter(at, '100 general surgery service')
-
-
-def set_filter(at: Atri, selected_filter: str):
-    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'data.json')
+def get_data(file_name):
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', file_name)
     json_file = open(file_path)
     data = json.load(json_file)
-    set_line_chart_data(at, data[selected_filter])
-    set_appointments(at, selected_filter)
-    set_year_to_data_comparison_data(at, data[selected_filter])
-    set_rolling_12_month_data(at, data[selected_filter])
-    get_fc_with_procedures(at, data[selected_filter])
-    pass
+    return data
 
 
-def set_line_chart_data(at: Atri, filtered_data):
+def get_activity(filtered_data):
     fin_dic = []
     for i, j in filtered_data.items():
         j['x'] = i
         fin_dic.append({a: b for a, b in j.items() if a != 'FCEs_With_Procedure'})
 
-    at.admissions.custom.data = fin_dic
-    pass
+    return fin_dic
 
 
-def set_rolling_12_month_data(at: Atri, filtered_data):
-    at.ttm_episodes.custom.cols = [
-                                    {"field": "id", "headerName": "Activity"},
-                                    {"field": "start_date", "headerName": "Oct 2020 - Sep 2021"},
-                                    {"field": "end_date", "headerName": "Oct 2021 - Sep 2022"},
-                                    {"field": "percentage_change", "headerName": "% Change"}
-                                    ]
+def get_appointments(filtered_data):
+    app_dict=[]
+    months = ['31JUL22', '31AUG22', '30SEP22']
+    for month in months:
+        canceled_app = filtered_data[month]["Total_Appointments"] - filtered_data[month]["Attended_Appointments"] - filtered_data[month]["DNA_Appointments"]
+        data_dict = {"x": month, 
+                    "Attended_Appointments": filtered_data[month]["Attended_Appointments"],
+                    "DNA_Appointments": filtered_data[month]["DNA_Appointments"],
+                    "Canceled_Appointments": canceled_app
+                    }
+        app_dict.append(data_dict)
+    return app_dict
+
+
+def get_procedures(filtered_data):
+    prep_data = {'FCE': 0, 'FCEs_With_Procedure': 0}
+    for i, j in filtered_data.items():
+        if i in get_specific_date_keys(21, ['JUL', 'AUG', 'SEP']):
+            for k in prep_data.keys():
+                prep_data[k] += filtered_data[i][k]
+    prep_data['FCE_without_procedure'] = (prep_data['FCE'] - prep_data['FCEs_With_Procedure'])/prep_data['FCE']
+    prep_data['FCEs_With_Procedure'] = prep_data['FCEs_With_Procedure'] / prep_data['FCE']
+    prep_data.pop('FCE')
+    data = [
+        [{"name": i, "value": j} for i, j in prep_data.items()]
+    ]
+    return data
+
+
+def get_ttm_activity(filtered_data):
+    cols = [
+            {"field": "id", "headerName": "Activity", "width":200},
+            {"field": "start_date", "headerName": "Oct 2020 - Sep 2021", "width":200},
+            {"field": "end_date", "headerName": "Oct 2021 - Sep 2022", "width":200},
+            {"field": "percentage_change", "headerName": "% Change"}
+            ]
 
     prep_data_1 = {'FCE':0, 'FCEs_With_Procedure':0, 'Ordinary_Admission_Episodes':0, 'FCE_DAY_CASES':0, 'FAE':0, 'EMERGENCY':0}
     for i,j in filtered_data.items():
@@ -57,12 +71,12 @@ def set_rolling_12_month_data(at: Atri, filtered_data):
             for k in prep_data_2.keys():
                 prep_data_2[k] += filtered_data[i][k]
 
-    at.ttm_episodes.custom.rows = [{"id": i, "start_date":prep_data_1[i], "end_date": prep_data_2[i], "percentage_change":(prep_data_2[i]-prep_data_1[i])*100/prep_data_1[i]} for i in prep_data_1.keys()]
-    pass
+    rows = [{"id": i, "start_date":prep_data_1[i], "end_date": prep_data_2[i], "percentage_change":(prep_data_2[i]-prep_data_1[i])*100/prep_data_1[i]} for i in prep_data_1.keys()]
+    
+    return cols, rows
 
-
-def set_year_to_data_comparison_data(at: Atri,filtered_data):
-    at.ytd_episodes.custom.cols = [
+def get_ytd_activity(filtered_data):
+    cols = [
         {"field": "id", "headerName": "Year to date comparison"},
         {"field": "start_date", "headerName": "April 2021-Sep 2021"},
         {"field": "end_date", "headerName": "April 2021 - Sep 2022"},
@@ -84,63 +98,10 @@ def set_year_to_data_comparison_data(at: Atri,filtered_data):
             for k in prep_data_2.keys():
                 prep_data_2[k] += filtered_data[i][k]
 
-    at.ytd_episodes.custom.rows = [{"id": i, "start_date": prep_data_1[i], "end_date": prep_data_2[i],
+    rows = [{"id": i, "start_date": prep_data_1[i], "end_date": prep_data_2[i],
                                     "percentage_change": (prep_data_2[i] - prep_data_1[i]) * 100 / prep_data_1[i]} for i
                                    in prep_data_1.keys()]
-    pass
-
-
-def get_fc_with_procedures(at: Atri, filtered_data):
-    prep_data = {'FCE': 0, 'FCEs_With_Procedure': 0}
-    for i, j in filtered_data.items():
-        if i in get_specific_date_keys(21, ['JUL', 'AUG', 'SEP']):
-            for k in prep_data.keys():
-                prep_data[k] += filtered_data[i][k]
-    prep_data['FCE_without_procedure'] = (prep_data['FCE'] - prep_data['FCEs_With_Procedure'])/prep_data['FCE']
-    prep_data['FCEs_With_Procedure'] = prep_data['FCEs_With_Procedure'] / prep_data['FCE']
-    prep_data.pop('FCE')
-    at.procedures.custom.data = [
-        [{"name": i, "value": j} for i, j in prep_data.items()]
-    ]
-    at.procedures.custom.options = [
-        # options for first circle
-        {
-            "cx": "50%",  # center of the circle's x
-            "cy": "50%",  # center of the circle's y
-            "showLabel": True,
-            "animate": False,
-        }
-    ]
-    at.procedures.custom.toolTip = {"show": True}
-    at.procedures.custom.legend = {"show": True}
-
-
-def set_appointments(at: Atri, selected_filter: str):
-    json_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'app_data.json')
-    json_file = open(json_file_path)
-    data = json.load(json_file)
-    filtered_data = data[selected_filter]
-    prep_data_1 = {'Total_Appointments': 0, 'Attended_Appointments': 0, 'DNA_Appointments': 0}
-    for i, j in filtered_data.items():
-        if i in get_specific_date_keys(22, ['JUL']):
-            for k in prep_data_1.keys():
-                prep_data_1[k] += filtered_data[i][k]
-    prep_data_1['x'] = '31JUL22'
-    prep_data_2 = {'Total_Appointments': 0, 'Attended_Appointments': 0, 'DNA_Appointments': 0}
-    for i, j in filtered_data.items():
-        if i in get_specific_date_keys(22, ['AUG']):
-            for k in prep_data_2.keys():
-                prep_data_2[k] += filtered_data[i][k]
-    prep_data_2['x'] = '31AUG22'
-    prep_data_3 = {'Total_Appointments': 0, 'Attended_Appointments': 0, 'DNA_Appointments': 0}
-    for i, j in filtered_data.items():
-        if i in get_specific_date_keys(22, ['SEP']):
-            for k in prep_data_3.keys():
-                prep_data_3[k] += filtered_data[i][k]
-    prep_data_3['x'] = '31SEP22'
-    fin_dic = [prep_data_1, prep_data_2, prep_data_3]
-    at.appointments.custom.data = fin_dic
-    pass
+    return cols, rows
 
 
 # Helper
--- controllers/routes/dashboard/main.py
+++ controllers/routes/dashboard/main.py
@@ -1,13 +1,56 @@
 from .atri import Atri
 from fastapi import Request, Response
 from atri_utils import *
-from backend.api import apply_dropdown_values, set_filter
+from backend.api import get_data, get_activity, get_appointments, get_procedures, get_ttm_activity, get_ytd_activity
+
+def set_filter(at:Atri, selected_filter:str, flag:str):
+
+    # Specialty
+    at.specialty.custom.text = selected_filter
+
+    # Extract data
+    data = get_data(file_name='data.json')
+
+    # Assign value to dropdown
+    if flag == "page_request":
+        at.specialty_dropdown.custom.values = list(data.keys())
+        at.specialty_dropdown.custom.selectedValue = selected_filter
+
+    # Activity line chart
+    at.admissions.custom.data = get_activity(data[selected_filter])
+
+    # Appointments
+    app_data = get_data(file_name='app_data.json')
+    at.appointments.custom.data = get_appointments(app_data[selected_filter])
+    at.appointments.custom.stacked = True
+
+    # FCEs with procedures
+    at.procedures.custom.data = get_procedures(data[selected_filter])
+    at.procedures.custom.options = [
+        # options for first circle
+        {
+            "cx": "50%",  # center of the circle's x
+            "cy": "50%",  # center of the circle's y
+            "showLabel": True,
+            "animate": False,
+        }
+    ]
+    at.procedures.custom.toolTip = {"show": True}
+    at.procedures.custom.legend = {"show": True}
+
+    # TTM activity
+    at.ttm_episodes.custom.cols = get_ttm_activity(data[selected_filter])[0]
+    at.ttm_episodes.custom.rows = get_ttm_activity(data[selected_filter])[1]
+
+    # YTD activity
+    at.ytd_episodes.custom.cols = get_ytd_activity(data[selected_filter])[0]
+    at.ytd_episodes.custom.rows = get_ytd_activity(data[selected_filter])[1]
 
 def init_state(at: Atri):
     """
-    This function is called everytime "Publish" button is hit in the editor.
+    This function is called every time "Publish" button is hit in the editor.
     The argument "at" is a dictionary that has initial values set from visual editor.
-    Changing values in this dictionary will modify the intial state of the app.
+    Changing values in this dictionary will modify the initial state of the app.
     """
     # describe column headers and the data type
     pass
@@ -17,8 +60,8 @@ def handle_page_request(at: Atri, req: Request, res: Response, query: str):
     """
     This function is called whenever a user loads this route in the browser.
     """
-    apply_dropdown_values(at)
-    pass
+    # Assign value to table
+    set_filter(at, selected_filter="100 general surgery service", flag="page_request")
 
 
 def handle_event(at: Atri, req: Request, res: Response):
@@ -26,8 +69,5 @@ def handle_event(at: Atri, req: Request, res: Response):
     This function is called whenever an event is received. An event occurs when user
     performs some action such as click button.
     """
-    if at.Dropdown1.onChange:
-        at.specialty.custom.text = at.Dropdown1.custom.selectedValue
-        set_filter(at, at.Dropdown1.custom.selectedValue)
-
-    pass
+    if at.specialty_dropdown.onChange:
+        set_filter(at, selected_filter=at.specialty_dropdown.custom.selectedValue, flag="handle_event")
